4ef9c4ec3dd147e9bdbc103896967ea7663863cc
KartikChugh <kartikchugh.kc@gmail.com>
2019-11-22 15:06:12 -0500

    Removes copying of initializer seeds if not user-provided
    Previously, using one of the provided, random Initializers for multiple layers would cause these layers to share the same initialization seed.
    
    In this commit, Initializer.copy() no longer copies the seed of the target Initializer, unless it has been set by the user via withSeed() . In effect, users can use LECUN_UNIFORM (for example) in multiple layers and each copy of this Initializer will still have a different seed.

--- src/kc/ml/jeras/initializers/AbstractRandom.java
+++ src/kc/ml/jeras/initializers/AbstractRandom.java
@@ -4,21 +4,29 @@ import java.util.Random;
 
 public abstract class AbstractRandom<T extends AbstractRandom<?>> extends Initializer<T> {
 
-    protected final Random rng = new Random();
+    private long seed;
+    protected final Random rng;
+    private boolean customSeed = false;
 
     AbstractRandom(Class<T> selfClass) {
         super(selfClass);
+        this.seed = System.nanoTime();
+        this.rng = new Random(seed);
     }
 
     public final T withSeed(long seed) {
+        this.seed = seed;
         rng.setSeed(seed);
+        customSeed = true;
         return this.self;
     }
 
     @Override
     public T copy() {
         final T copy = super.copy();
-        copy.withSeed(seed);
+        if (customSeed) {
+            copy.withSeed(seed);
+        }
         return copy;
     }
 
